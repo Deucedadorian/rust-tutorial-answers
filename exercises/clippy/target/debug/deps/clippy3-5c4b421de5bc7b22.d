306c744d7c0f9d373551d84ba306fe500cd9c9cd
Dylan Cole <logboarddjc@gmail.com>
2022-11-29 15:02:33 -0500

    add solution from stack overflow -> https://stackoverflow.com/questions/73294897/correct-answer-at-rustlings-course-but-not-happy-with-it

---
+++ exercises/clippy/target/debug/deps/clippy3-5c4b421de5bc7b22.d
@@ -0,0 +1,7 @@
+/Users/dylan/repos/rustlings/exercises/clippy/target/debug/deps/clippy3-5c4b421de5bc7b22.rmeta: clippy3.rs
+
+/Users/dylan/repos/rustlings/exercises/clippy/target/debug/deps/clippy3-5c4b421de5bc7b22.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
